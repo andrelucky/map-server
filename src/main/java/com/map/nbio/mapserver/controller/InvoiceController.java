910bc4e001bff70f442a7097415194135ecd826a
AndreLuckyanto <andreluckyanto@gmail.com>
2019-04-02 23:41:33 +0700

    test always sent event

--- src/main/java/com/map/nbio/mapserver/controller/InvoiceController.java
+++ src/main/java/com/map/nbio/mapserver/controller/InvoiceController.java
@@ -7,8 +7,12 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 import java.text.ParseException;
+import java.time.Duration;
+import java.util.Date;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("api/invoice/")
@@ -21,11 +25,20 @@ public class InvoiceController {
             produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     public Flux<Invoice> all() throws ParseException {
-        return invoiceService.findAll();
+//        return invoiceService.findAll();
+        Flux<Invoice> eventFlux = Flux.fromStream(Stream.generate(Invoice::new));
+
+        Flux<Long> durationFlux = Flux.interval(Duration.ofSeconds(
+                30
+        ));
+
+
+        return
+                Flux.zip(eventFlux, durationFlux).map(Tuple2::getT1);
     }
 
     @PostMapping(path = "save")
-    public Mono<Invoice> save(@RequestBody Invoice invoice){
+    public Mono<Invoice> save(@RequestBody Invoice invoice) {
         return invoiceService.save(invoice);
     }
 }
